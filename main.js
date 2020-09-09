22f802a88a7b090aa0da33f7ff3a84f746738f2a
Jose Joya <ing.jose.joya@gmail.com>
2020-09-09 09:16:19 -0500

    update imports

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 const fs = require('fs');
-const vueTemplate = require('./src/templates/vue');
-const reactTemplate = require('./src/templates/react');
 const config = require('./props.json');
 const Logger = require('./src/Logger');
 const frameworks = require('./src/FrameworkManager');
+
 const logger = new Logger();
 
 const frameworkType = process.argv.pop();
 
 logger.info(`Using ${frameworkType} to transform`);
 
-
-fs.readdir(`${__dirname}/icons`, (err, files) => {
-  if (!err) {
-    frameworks[frameworkType](files, config);
-  }
-});
+try {
+  fs.readdir(`${__dirname}/icons`, (err, files) => {
+    if (!err) {
+      frameworks[frameworkType](files, config);
+    } else {
+      throw new Error(`You have to put your svg icons inside ${__dirname}/icons folder.`);
+    }
+  });
+} catch (err) {
+  logger.error(err.message);
+}
--- src/FrameworkManager/index.js
+++ src/FrameworkManager/index.js
@@ -1,56 +1,63 @@
+const fs = require('fs');
+const vueTemplate = require('../templates/vue');
+const reactTemplate = require('../templates/react');
+const Logger = require('../Logger');
+
+const logger = new Logger();
+
 const frameworks = {
-    vue(files, configFile) {
-        logger.info('Creating vue folder');
-        if (!fs.existsSync('./components/vue')) {
-            fs.mkdirSync('./components/vue', { recursive: true });
-        }
-        files.forEach((file) => {
-            if (configFile.ignoredFiles.indexOf(file) === -1) {
-                const fileContent = fs.readFileSync(`${__dirname}/icons/${file}`, {
-                    encoding: 'utf8'
-                });
-                if (fileContent) {
-                    vueTemplate
-                        .transform({ fileContent, fileName: file.replace(/ +/g, '_').replace(/.svg/g, '') })
-                        .then((logInfo) => {
-                            if (logInfo) {
-                                logger.done(logInfo);
-                            }
-                        })
-                        .catch((error) => {
-                            logger.error(error);
-                        });
-                }
-            }
-        });
-        vueTemplate.save(files, configFile);
-    },
-    react(files, configFile) {
-        logger.info('Creating react folder');
-        if (!fs.existsSync('./components/react')) {
-            fs.mkdirSync('./components/react', { recursive: true });
-        }
-        files.forEach((file) => {
-            if (configFile.ignoredFiles.indexOf(file) === -1) {
-                const fileContent = fs.readFileSync(`${__dirname}/icons/${file}`, {
-                    encoding: 'utf8'
-                });
-                if (fileContent) {
-                    reactTemplate
-                        .transform({ fileContent, fileName: file.replace(/ +/g, '_').replace(/.svg/g, '') })
-                        .then((logInfo) => {
-                            if (logInfo) {
-                                logger.done(logInfo);
-                            }
-                        })
-                        .catch((error) => {
-                            logger.error(error);
-                        });
-                }
-            }
-        });
-        reactTemplate.save(files, configFile);
+  vue (files, configFile) {
+    logger.info('Creating vue folder');
+    if (!fs.existsSync('./components/vue')) {
+      fs.mkdirSync('./components/vue', { recursive: true });
     }
+    files.forEach((file) => {
+      if (configFile.ignoredFiles.indexOf(file) === -1) {
+        const fileContent = fs.readFileSync(`${__dirname}/icons/${file}`, {
+          encoding: 'utf8'
+        });
+        if (fileContent) {
+          vueTemplate
+            .transform({ fileContent, fileName: file.replace(/ +/g, '_').replace(/.svg/g, '') })
+            .then((logInfo) => {
+              if (logInfo) {
+                logger.done(logInfo);
+              }
+            })
+            .catch((error) => {
+              logger.error(error);
+            });
+        }
+      }
+    });
+    vueTemplate.save(files, configFile);
+  },
+  react (files, configFile) {
+    logger.info('Creating react folder');
+    if (!fs.existsSync('./components/react')) {
+      fs.mkdirSync('./components/react', { recursive: true });
+    }
+    files.forEach((file) => {
+      if (configFile.ignoredFiles.indexOf(file) === -1) {
+        const fileContent = fs.readFileSync(`${__dirname}/icons/${file}`, {
+          encoding: 'utf8'
+        });
+        if (fileContent) {
+          reactTemplate
+            .transform({ fileContent, fileName: file.replace(/ +/g, '_').replace(/.svg/g, '') })
+            .then((logInfo) => {
+              if (logInfo) {
+                logger.done(logInfo);
+              }
+            })
+            .catch((error) => {
+              logger.error(error);
+            });
+        }
+      }
+    });
+    reactTemplate.save(files, configFile);
+  }
 };
 
 module.exports = frameworks;
