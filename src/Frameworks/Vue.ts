e43f7f19bafb2faf7303bb604ae1792bb8f416a0
Jose Joya <ing.jose.joya@gmail.com>
2020-09-17 00:00:40 -0500

    Added :Refactor over react Module

--- src/Frameworks/Vue.ts
+++ src/Frameworks/Vue.ts
@@ -7,105 +7,109 @@ import { JSDOM } from 'jsdom';
 import template from './Templates/vueTemplate.html';
 
 interface PropsType {
-    [key:string]:{ type: any, default: any,  }
+    [key: string]: { type: any, default: any, }
 }
 
 export default class Vue implements Framework {
     private config?: Config;
-    private iconsFolder? : string;
+    private iconsFolder?: string;
     name: string;
 
-    constructor(private basePath:string ,private options: CliOptions){
+    constructor(private basePath: string, private options: CliOptions) {
         this.name = 'Vue';
     }
 
-    run(config: Config){
+    run(config: Config): void {
         this.config = config;
         const files = this.loadFiles();
-        if(files.length > 0){
-            mkdirSync(`${this.basePath}/${this.options.oD||'components'}/${this.name}`, { recursive: true});
-            const vueFiles = files.map(file=>{
-                 return {
-                     fileName: file.split('/').pop()?.replace('.svg','.vue'),
-                     content: this.processFile(file),
-                 };
+        if (files.length > 0) {
+            mkdirSync(`${this.basePath}/${this.options.oD || 'components'}/${this.name}`, { recursive: true });
+            const vueFiles = files.map(file => {
+                return {
+                    fileName: file.split('/').pop()?.replace('.svg', '.vue'),
+                    content: this.processFile(file),
+                };
             });
-            vueFiles.forEach(({fileName,content})=>{
-                writeFileSync(`${this.basePath}/${this.options.oD || 'components'}/${this.name}/${fileName}`,content ,{ encoding:'utf-8' });
+            vueFiles.forEach(({ fileName, content }) => {
+                writeFileSync(`${this.basePath}/${this.options.oD || 'components'}/${this.name}/${fileName}`, content, { encoding: 'utf-8' });
             });
         } else {
             console.log('This folder does not have any SVG file');
         }
-        
+
     }
 
-    private loadFiles(): string[]{
+    private loadFiles(): string[] {
         this.iconsFolder = this.options.iF || 'icons';
-        const rawFiles = readdirSync(Path.join(this.basePath,this.iconsFolder),{ encoding:'utf-8', withFileTypes: true, });
-        const filteredFiles = rawFiles.filter(file=>file.isFile() && file.name.endsWith('.svg') && file);
-        return filteredFiles.map(file=>this.basePath+'/'+this.iconsFolder+'/'+file.name);
+        const rawFiles = readdirSync(Path.join(this.basePath, this.iconsFolder), { encoding: 'utf-8', withFileTypes: true, });
+        const filteredFiles = rawFiles.filter(file => file.isFile() && file.name.endsWith('.svg') && file);
+        return filteredFiles.map(file => this.basePath + '/' + this.iconsFolder + '/' + file.name);
     }
 
-    private processFile(filePath : string):string {
+    private processFile(filePath: string): string {
 
         let vueTemplate = `${template}`;
-        let props: PropsType = {};
-        const fileContent = readFileSync(filePath, { encoding:'utf-8' });
+        const props: PropsType = {};
+        const fileContent = readFileSync(filePath, { encoding: 'utf-8' });
         const htmlObject = new JSDOM('<html><head></head><body></body></html>');
-        
+
         htmlObject.window.document.body.innerHTML = fileContent;
-        
+
         const svgObject = htmlObject.window.document.body.firstChild as HTMLElement;
 
         this.processElement(svgObject, props);
-        this.processChildNodes(props,svgObject.children);
+        this.processChildNodes(props, svgObject.children);
 
-        vueTemplate = vueTemplate?.replace(`{{${VUE_STATIC_TAGS.TEMPLATE_HTML}}}`,svgObject.outerHTML);
-        vueTemplate = vueTemplate?.replace(`{{${VUE_STATIC_TAGS.COMPONENT_NAME}}}`,filePath.split('/').pop()!.replace('.svg','').replace(' ','_'));
-        const propsJson = JSON.stringify( props, null, 2)
-            .replace(/('|")String('|")/g,'String')
-            .replace(/('|")Array('|")/g,'Array');
-        vueTemplate = vueTemplate?.replace(`{{${VUE_STATIC_TAGS.PROPS_CONTENT}}}`,propsJson );
-        
+        vueTemplate = vueTemplate?.replace(`{{${VUE_STATIC_TAGS.TEMPLATE_HTML}}}`, svgObject.outerHTML);
+        vueTemplate = vueTemplate?.replace(`{{${VUE_STATIC_TAGS.COMPONENT_NAME}}}`, (filePath.split('/').pop() || "").replace('.svg', '').replace(' ', '_'));
+        const propsJson = JSON.stringify(props, null, 2)
+            .replace(/('|")String('|")/g, 'String')
+            .replace(/('|")Array('|")/g, 'Array');
+        vueTemplate = vueTemplate?.replace(`{{${VUE_STATIC_TAGS.PROPS_CONTENT}}}`, propsJson);
+
         return vueTemplate;
     }
-    private processChildNodes(props: PropsType, children?: HTMLCollection){
-        if(children){
-            for(let i=0; i< children.length; i+=1){
+    private processChildNodes(props: PropsType, children?: HTMLCollection): void {
+        if (children) {
+            for (let i = 0; i < children.length; i += 1) {
                 const child = children.item(i);
-                if(child){
+                if (child) {
                     this.processElement(child as HTMLElement, props);
                 }
             }
         }
     }
-    private processElement(element: HTMLElement, props: PropsType){
-        Object.values(element.attributes).forEach(attribute=>{ 
-            if(!this.config?.attributesBlackList.includes(attribute.name)){
+    private processElement(element: HTMLElement, _props: PropsType): void {
+        Object.values(element.attributes).forEach(attribute => {
+            if (!this.config?.attributesBlackList.includes(attribute.name)) {
                 const attrName = attribute.name;
                 const defaultValue = attribute.value;
                 element.removeAttribute(attrName);
-                if(attrName === 'fill' || attrName === 'stroke'){
-                    if(!props[attrName]){
-                        props[attrName] = {
-                            type: 'Array',
-                            default: [defaultValue],
-                        };
-                    }
-                    if(!props[attrName].default.includes(defaultValue)){
-                        props[attrName].default.push(defaultValue);
-                    }
-                    element.setAttribute(':'+attrName,`${attrName}[${props[attrName].default.indexOf(defaultValue)}]` );
-                } else {
-                    props[attrName] = {
-                        type: 'String',
-                        default: defaultValue,
-                    }
-                    element.setAttribute(':'+attrName,attrName);
-                }
-                
+                this.fillProps(_props, attrName, defaultValue, element);
             }
-        },this);
+        }, this);
+    }
+
+    private fillProps(_props: PropsType, attrName: string, defaultValue: string, element: HTMLElement): void {
+        const props = _props;
+        if (attrName === 'fill' || attrName === 'stroke') {
+            if (!props[attrName]) {
+                props[attrName] = {
+                    type: 'Array',
+                    default: [defaultValue],
+                };
+            }
+            if (!props[attrName].default.includes(defaultValue)) {
+                props[attrName].default.push(defaultValue);
+            }
+            element.setAttribute(':' + attrName, `${attrName}[${props[attrName].default.indexOf(defaultValue)}]`);
+        } else {
+            props[attrName] = {
+                type: 'String',
+                default: defaultValue,
+            }
+            element.setAttribute(':' + attrName, attrName);
+        }
     }
 
 }
--- src/Frameworks/types.ts
+++ src/Frameworks/types.ts
@@ -2,11 +2,17 @@ import { Config } from "../../types";
 
 export interface Framework {
     name: string;
-    run( config:Config): void;
+    run(config: Config): void;
 }
 
 export enum VUE_STATIC_TAGS {
-    TEMPLATE_HTML='TEMPLATE_HTML',
-    COMPONENT_NAME='COMPONENT_NAME',
-    PROPS_CONTENT='PROPS_CONTENT',
+    TEMPLATE_HTML = 'TEMPLATE_HTML',
+    COMPONENT_NAME = 'COMPONENT_NAME',
+    PROPS_CONTENT = 'PROPS_CONTENT',
+}
+
+export enum REACT_STATIC_TAGS {
+    ICON_NAME = "ICON_NAME",
+    PROPS_DEFAULT = "PROPS_DEFAULT",
+    JSX_TEMPLATE = "JSX_TEMPLATE"
 }
